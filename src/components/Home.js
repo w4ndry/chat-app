3466b01b6e9564cef821826e7c66fa90c0dbd27d
w4ndry <eriwandri@gmail.com>
2017-03-28 22:36:48 +0700

    Create Home screen

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+	View,
+	Text,
+} from 'react-native';
+
+class Home extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>
+					Hello Home
+				</Text>
+			</View>
+		);
+	}
+}
+
+export default Home;
